b88a077ad06242970a7b4d7291bacea3907d4557
AzureAD\AnkanMookherjee <ankan.mookherjee@audioagent.com>
2017-04-30 16:25:10 +0100

    work in progress nothing to show

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('\'Allo \'Allo!');
+var dtlApp = angular.module('dtlApp', ['ngRoute']);
+
+	// configure our routes
+	dtlApp.config(function($routeProvider) {
+		$routeProvider
+
+			// route for the home page
+			.when('/home', {
+				templateUrl : 'views/home.html',
+				controller  : 'mainController'
+			})			
+	});
+
+
+	// create the controller and inject Angular's $scope
+	dtlApp.controller('mainController', function($scope) {
+		// create a message to display in our view
+		$scope.message = 'Everyone come and see how good I look!';
+	});
+
