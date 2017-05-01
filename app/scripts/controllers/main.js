712bcbb9fadd739b5da25da6c22dda005a9c0e47
AzureAD\AnkanMookherjee <ankan.mookherjee@audioagent.com>
2017-05-01 21:24:29 +0100

    setting up the gory ugly view and model before we embark on the filtering journey

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,14 +3,16 @@
 angular.module('dtlApp')
 .controller('mainController',  ['$routeParams', '$scope', '$http',function($routeParams,$scope,$http) {
 
-	$scope.choice = [];
+	$scope.response = {};
 	$scope.busy = true;
 	$scope.error = '';
 
-	$scope.message = 'Everyone come and see how good I look!';
+	// $scope.message = 'Everyone come and see how good I look!';
 	$http.get('../deals.json')
 	.then(function(response){
-		angular.copy(response.data, $scope.choice);
+		$scope.response = response.data; 
+		//angular.copy(response.data, $scope.deals);
+				console.log($scope.response.deals);
 	},function(err){
 		console.log(err);
 		$scope.error = err;
