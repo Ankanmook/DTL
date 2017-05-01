e2fcaae6a599eeb72364e33385b41a6190617021
AzureAD\AnkanMookherjee <ankan.mookherjee@audioagent.com>
2017-05-01 21:27:48 +0100

    cleanup here and there

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,16 +3,14 @@
 angular.module('dtlApp')
 .controller('mainController',  ['$routeParams', '$scope', '$http',function($routeParams,$scope,$http) {
 
-	$scope.response = {};
+	$scope.deals = {};
 	$scope.busy = true;
 	$scope.error = '';
 
 	// $scope.message = 'Everyone come and see how good I look!';
 	$http.get('../deals.json')
 	.then(function(response){
-		$scope.response = response.data; 
-		//angular.copy(response.data, $scope.deals);
-				console.log($scope.response.deals);
+		$scope.deals = response.data.deals;
 	},function(err){
 		console.log(err);
 		$scope.error = err;
