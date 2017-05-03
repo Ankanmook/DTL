e0fcd1c84309d76b94eff9b3d9b5f04823f9c96b
AzureAD\AnkanMookherjee <ankan.mookherjee@audioagent.com>
2017-05-03 09:02:45 +0100

    more changes to follow

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,17 +3,18 @@
 angular.module('dtlApp')
 .controller('mainController',  ['$routeParams', '$scope', '$http',function($routeParams,$scope,$http) {
 
-	$scope.deals = {};
+	$scope.deals =[];
 	$scope.busy = true;
 	$scope.error = '';
+	$scope.Filter = {TV: "TV", Broadband: "Broadband", Phone: "Phone" };
 
 	// $scope.message = 'Everyone come and see how good I look!';
 	$http.get('../deals.json')
 	.then(function(response){
-		$scope.deals = response.data.deals;
+		angular.copy(response.data.deals, $scope.deals);
+		// console.log($scope.deals);
+		// console.log($scope);
 
-		console.log($scope.deals);
-		console.log($scope);
 	},function(err){
 		console.log(err);
 		$scope.error = err;
@@ -23,18 +24,38 @@ angular.module('dtlApp')
 	});
 
 	$scope.filterDeals = function(deal){
-		console.log(deal.productTypes);
-		console.log($scope.Filter);
 
-		console.log(deal.productTypes.indexOf($scope.Filter.TV !== -1)||
-		deal.productTypes.indexOf($scope.Filter.Phone !== -1) ||
-			deal.productTypes.indexOf($scope.Filter.Broadband !== -1));
+		//console.log(deal.productTypes);
+		//console.log($scope.Filter);
+		//console.log(deal.productTypes.indexOf($scope.Filter.TV));
+		//console.log(deal.productTypes.indexOf($scope.Filter.Phone));
+		//console.log(deal.productTypes.indexOf($scope.Filter.Broadband));
 
-		return deal.productTypes.indexOf($scope.Filter.TV !== -1)||
-		deal.productTypes.indexOf($scope.Filter.Phone !== -1) ||
-			deal.productTypes.indexOf($scope.Filter.Broadband !== -1);
+		if(deal.productTypes.indexOf($scope.Filter.TV) > -1)
+		{
+			return true;
+		}
+		else if(deal.productTypes.indexOf($scope.Filter.Phone) > -1)
+		{
+			return true;
+		}
+		else if(deal.productTypes.indexOf($scope.Filter.TV) > -1)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+
+		// console.log(deal.productTypes.indexOf($scope.Filter.TV !== -1)||
+		// deal.productTypes.indexOf($scope.Filter.Phone !== -1) ||
+		// 	deal.productTypes.indexOf($scope.Filter.Broadband !== -1));
+
+		// return deal.productTypes.indexOf($scope.Filter.TV !== -1) &&
+		// deal.productTypes.indexOf($scope.Filter.Phone !== -1) &&
+		// 	deal.productTypes.indexOf($scope.Filter.Broadband !== -1);
 		};
-
 
     }]);
 })();
