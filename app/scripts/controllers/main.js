8123e1043d3f5e1f4c444f7c371b5288e602d99f
AzureAD\AnkanMookherjee <ankan.mookherjee@audioagent.com>
2017-05-03 09:11:39 +0100

    filter 2 setup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,11 @@ angular.module('dtlApp')
 		$scope.error = '';
 		$scope.Filter = {TV: "TV", Broadband: "Broadband", Phone: "Phone" };
 
+		$scope.Filter2 = {
+			productTypes:{TV: "TV", Broadband: "Broadband", Phone: "Phone" },
+			speed : {Any: "Any", MobileDate: "5GB", Fast: "52MB"}
+		}
+
 		// $scope.message = 'Everyone come and see how good I look!';
 		$http.get('../deals.json')
 		.then(function(response){
@@ -23,7 +28,12 @@ angular.module('dtlApp')
 			$scope.busy = false;
 		});
 
-		$scope.filterDeals = function(deal){	
+		$scope.filterDeals = function(deal){
+
+			//broadband only deals
+			//broadband and tv only deals
+			//broadband and mobile
+
 			if(deal.productTypes.indexOf($scope.Filter.Broadband) > -1)
 			{
 				return true;
